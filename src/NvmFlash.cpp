3250e19c6ef65f021b05997fe7507bd52423af0e
Cristian Maglie <c.maglie@bug.st>
2014-08-17 10:28:28 +0200

    Use of portable stdlib.h include for malloc function.

--- src/NvmFlash.cpp
+++ src/NvmFlash.cpp
@@ -18,7 +18,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdint.h>
-#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <memory>
 #include <iostream>
 #include <exception>
