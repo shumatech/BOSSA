e9530b874a588f76ab87b6180177eb7404bb0927
Kyle Evans <kevans91@ksu.edu>
2017-03-30 15:43:21 -0500

    Add arduino 1200bps erase flag (-a)

--- src/bossac.cpp
+++ src/bossac.cpp
@@ -66,6 +66,7 @@ public:
     bool help;
     bool forceUsb;
     string forceUsbArg;
+    bool arduinoErase;
 
     int readArg;
     int offsetArg;
@@ -92,6 +93,7 @@ BossaConfig::BossaConfig()
     info = false;
     help = false;
     forceUsb = false;
+    arduinoErase = false;
 
     readArg = 0;
     offsetArg = 0;
@@ -248,6 +250,11 @@ static Option opts[] =
       'R', "reset", &config.reset,
       { ArgNone },
       "reset CPU (if supported)"
+    },
+    {
+      'a', "arduino_erase", &config.arduinoErase,
+      { ArgNone },
+      "erase and reset via Arduino 1200 baud hack (cannot be used with port autodetection)"
     }
 };
 
@@ -316,6 +323,12 @@ main(int argc, char* argv[])
         return help(argv[0]);
     }
 
+    if (config.arduinoErase && !config.port)
+    {
+        fprintf(stderr, "%s: port must be specified for Arduino 1200bps erase hack\n", argv[0]);
+        return help(argv[0]);
+    }
+
     if (config.read || config.write || config.verify)
     {
         if (args == argc)
@@ -370,6 +383,18 @@ main(int argc, char* argv[])
                 fprintf(stderr, "Invalid USB value: %s\n", config.forceUsbArg.c_str());
                 return 1;
             }
+        }
+
+        if (config.arduinoErase)
+        {
+            SerialPort::Ptr port;
+            if (config.forceUsb)
+                port = portFactory.create(config.portArg, isUsb);
+            else
+                port = portFactory.create(config.portArg);
+
+            port->open(1200);
+            port->close();
         }
 
         if (config.port)
